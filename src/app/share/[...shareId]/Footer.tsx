18a61c65af4736834f1e331dba72db16b17e5d18
Dimitar Yanakiev <dyanakiev@users.noreply.github.com>
2025-03-11 11:26:16 +0200

    Update Footer.tsx

--- src/app/share/[...shareId]/Footer.tsx
+++ src/app/share/[...shareId]/Footer.tsx
@@ -4,9 +4,9 @@ import styles from './Footer.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href={HOMEPAGE_URL}>
+{/*       <a href={HOMEPAGE_URL}>
         <b>umami</b> {`v${CURRENT_VERSION}`}
-      </a>
+      </a> */}
     </footer>
   );
 }
